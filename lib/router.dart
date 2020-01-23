dfdd65c0a103c8cb8dc85148c7df19a887025021
Cheshta Kwatra <cheshta.kwatra@gmail.com>
2020-01-23 23:50:53 +0530

    UI Modifications (#13)
    ui modifications

--- lib/router.dart
+++ lib/router.dart
@@ -1,20 +1,23 @@
-
+import 'package:eliana_chatbot/home.dart';
+import 'package:eliana_chatbot/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:eliana_chatbot/main.dart';
-import 'package:eliana_chatbot/chatpage.dart';
-const String FACTS_DIALOGFLOW = "FACTS_DIALOGFLOW";
+import 'package:eliana_chatbot/chat_page.dart';
+
+const String chat_page = "CHAT_PAGE";
+const String home_page = "HOME_PAGE";
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
-  switch(routeSettings.name) {
+  switch (routeSettings.name) {
     case '/':
-      return MaterialPageRoute(builder: (context) => MyHomePage());
+      return MaterialPageRoute(builder: (context) => SplashScreen());
       break;
-    case FACTS_DIALOGFLOW:
+    case home_page:
+      return MaterialPageRoute(builder: (context) => HomePage());
+      break;
+    case chat_page:
       return MaterialPageRoute(builder: (context) => ChatScreen());
       break;
     default:
-    return MaterialPageRoute(builder: (context) => MyHomePage());
+      return MaterialPageRoute(builder: (context) => HomePage());
   }
-  
-
-  }
+}
